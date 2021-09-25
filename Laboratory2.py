446d6ea80acae1c967ddbc38f85bfd2c6d842479
Hans Sioson <hansxgabriel@gmail.com>
2021-09-25 17:56:11 +0000

    3rd commit

--- Laboratory2.py
+++ Laboratory2.py
@@ -14,3 +14,18 @@ elif avg<75:
     print("FAILED")
 else:
     print("Invalid")
+
+    if avg>=99 and avg<=100:
+    print("A")
+elif avg>=90 and avg<=98:
+    print("B")
+elif avg>=80 and avg<=89:
+    print("C")
+elif avg>=71 and avg<=79:
+    print("D")
+elif avg>=61 and avg<=70:
+    print("E")
+elif avg<=60:
+    print("F")
+else:
+    print("Invalid Input!")
